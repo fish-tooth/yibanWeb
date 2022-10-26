a9f42fe2ae8cf50faa851a9b0f1d113b435f43c2
jjk <2361491843@qq.com>
2022-10-26 21:02:21 +0800

    修复路径写错的问题

--- yiban/src/components/HelloWorld.vue
+++ yiban/src/components/HelloWorld.vue
@@ -1,13 +1,13 @@
 <script setup lang="ts">
-import Header from './common/Header.vue';
+import Header from "./common/Header.vue";
 import { ref } from "vue";
-import API from '../axios/index'
-
-const res=await API.get('/api/getData')
-console.log(res);
-
+import API from "../axios/index";
 const msg = ref("你好");
 
+(async () => {
+  const res = await API.get("/api/getData");
+  console.log(res);
+})();
 </script>
 
 <template>
--- yiban/src/router/index.ts
+++ yiban/src/router/index.ts
@@ -1,26 +1,26 @@
 import {
-    createWebHistory,
-    createWebHashHistory,
-    createRouter,
-  } from "vue-router";
-  
-  const routes = [
-    {
-      path: "/:catchAll(.*)",
-      component: () => import("../components/notFound.vue"),
-    },
-    {
-      path: "/",
-      name: "helloworld",
-      component: () => import("../components/Helloworld.vue"),
-      // chirlden:[]
-    },
-  ];
-  
-  const router = createRouter({
-    history: createWebHashHistory(),
-    // history: createWebHistory(),
-    routes,
-  });
-  
-  export default router;
+  createWebHistory,
+  createWebHashHistory,
+  createRouter,
+} from "vue-router";
+
+const routes = [
+  {
+    path: "/:catchAll(.*)",
+    component: () => import("../components/notFound.vue"),
+  },
+  {
+    path: "/",
+    name: "HelloWorld",
+    component: () => import("../components/HelloWorld.vue"),
+    // chirlden:[]
+  },
+];
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  // history: createWebHistory(),
+  routes,
+});
+
+export default router;
