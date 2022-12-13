cd21e08dcec53ea668072d83b0bd0ece07762dee
lyushyan <1595209732@qq.com>
2022-12-13 19:28:09 +0800

    调整首页样式

--- yiban/src/components/common/Header.vue
+++ yiban/src/components/common/Header.vue
@@ -1,18 +1,16 @@
 <template>
   <div class="head">
     <!-- <div class="logo"> -->
-    <img
-      class="logo headLogo"
+    <img class="logo"
       src="https://img.js.design/assets/img/636513ba0b26ac3f6170fccb.png#078ed25b566af1b2b66771bf68cb21de"
-      alt="图片加载失败"
-    />
-    <div class="name headName">以伴青少年<br />发展中心</div>
+      alt="图片加载失败" />
+    <div class="name">以伴青少年<br />发展中心</div>
     <!-- </div> -->
     <div class="index">
-      <div class="headList headAbout">关于我们</div>
-      <div class="headList headNews">新闻中心</div>
-      <div class="headList headRecruit">人才招聘</div>
-      <div class="headList headContact">联系我们</div>
+      <div class="headAbout">关于我们</div>
+      <div class="headNews">新闻中心</div>
+      <div class="headRecruit">人才招聘</div>
+      <div class="headContact">联系我们</div>
     </div>
   </div>
 </template>
@@ -20,120 +18,69 @@
 <script setup lang="ts"></script>
 
 <style lang="scss" scoped>
-.logo {
-  width: 40px;
-  height: 40px;
-  opacity: 1;
+// font-family
+$family1: 阿里妈妈数黑体;
+$family2: 阿里巴巴惠普体regular;
+$family3: 思源黑体;
+$family4: 优设标题圆;
 
+@mixin size-position($width: auto, $height: auto, $left: auto, $top: auto) {
+  width: $width;
+  height: $height;
+  left: $left;
+  top: $top;
   position: absolute;
-}
-
-.name {
-  width: 90px;
-  height: 44px;
-  opacity: 1;
-  /** 文本1 */
-  font-size: 18px;
-  font-weight: 400;
-  letter-spacing: 0px;
-  /* line-height: 0px; */
-  text-align: left;
-  vertical-align: top;
-
-  /* float: left; */
-  position: absolute;
-  line-height: 22px;
-  font-family: 阿里妈妈数黑体;
 }
 
 .head {
-  left: 0px;
-  top: 0px;
-  width: 1440px;
-  height: 84px;
-  opacity: 1;
+  // @include size-position(1440px, 84px, 0, 0);
+  @include size-position(1440px, 84px, 0, 0);
   background: rgba(255, 255, 255, 1);
-
-  position: absolute;
   z-index: 1;
-}
 
-.headLogo {
-  left: 38px;
-  top: 24px;
-}
 
-.headName {
-  left: 86px;
-  top: 20px;
-  color: rgba(0, 0, 0, 1);
-}
+  .logo {
+    @include size-position(40px, 40px, 38px, 24px);
+  }
 
-/* 
-.index {
-    height: 29px;
-    width: 400px;
-    float: right;
-    background-color: rgb(199, 153, 153);
-    margin-right: 100px;
-    display: flex;
-} */
-
-/* .list {
-    left: 1075.33px;
-    top: 28px;
-    width: 80px;
-    height: 29px;
-    opacity: 1; */
-/** 文本1 */
-/* font-size: 20px;
-    font-weight: 400;
-    letter-spacing: 0px;
-    line-height: 29px;
+  .name {
+    @include size-position(90px, 44px, 86px, 20px);
     color: rgba(0, 0, 0, 1);
+    font: 400 18px/22px $family1;
     text-align: left;
-    vertical-align: middle;
-    float: right;
-    margin-right: 20px;
+    vertical-align: top;
+  }
+}
 
-
-} */
-
-.headList {
+%headList {
   top: 28px;
   width: 80px;
   height: 29px;
-  opacity: 1;
-  /** 文本1 */
-  font-size: 20px;
-  font-weight: 400;
-  letter-spacing: 0px;
-  /* line-height: 0px; */
+  font: 400 20px/29px $family3;
   color: rgba(0, 0, 0, 1);
   text-align: left;
   vertical-align: middle;
-
-  /* float: right; */
   display: inline-block;
-  /* display: block; */
-  position: absolute;
-  line-height: 29px;
-  font-family: 思源黑体;
+  position: $position;
 }
 
 .headAbout {
+  @extend %headList;
   left: 970px;
 }
 
 .headNews {
+  @extend %headList;
   left: 1075.33px;
 }
 
 .headRecruit {
+  @extend %headList;
   left: 1180.67px;
 }
 
 .headContact {
+  @extend %headList;
   left: 1286px;
 }
 </style>
--- yiban/src/components/home.vue
+++ yiban/src/components/home.vue
@@ -9,6 +9,7 @@
           <div class="learnMore">了解更多</div>
           <div class="arrow">
             <!-- <i class="right iconfont">&#xe62c;</div> -->
+            <div class="right"></div>
           </div>
         </div>
       </div>
@@ -32,56 +33,63 @@
       </div>
 
       <div class="numberBackground">
-        <div class="bigNumber number1">10087</div>
-        <div class="smallNumber number11">累计陪伴学生10087人</div>
+        <div class="number1">10087</div>
+        <div class="number11">累计陪伴学生10087人</div>
 
-        <div class="bigNumber number2">2019</div>
-        <div class="smallNumber number21">累计参加志愿者2019人</div>
+        <div class="number2">2019</div>
+        <div class="number21">累计参加志愿者2019人</div>
 
-        <div class="bigNumber number3">215</div>
-        <div class="smallNumber number31">累计开展专项辅导陪伴项目215个</div>
+        <div class="number3">215</div>
+        <div class="number31">累计开展专项辅导陪伴项目215个</div>
       </div>
     </div>
 
     <div class="coreBusiness">
       <div class="coreTitle">我们的核心工作</div>
-      <div class="spread spread01"></div>
-      <div class="spread spread02"></div>
-      <div class="spread spread03"></div>
-      <div class="spread spread04"></div>
-      <div class="spread spread05"></div>
-      <div class="spread spread06"></div>
+      <div class="spread01"></div>
+      <div class="spread02"></div>
+      <div class="spread03"></div>
+      <div class="spread04"></div>
+      <div class="spread05"></div>
+      <div class="spread06"></div>
     </div>
 
     <div class="runningProject">
       <div class="runningTitle">我们的行动正在发生</div>
       <div class="project1">
-        <div class="upPage upPage1"></div>
-        <div class="underPage underPage1">
+        <div class="upPage1"></div>
+        <div class="underPage1">
           <div class="projectTitle">以伴的项目名称</div>
+          <div class="content">
+            项目的详细介绍分享形式、分享动机、分享场景4个维度来聊聊「社交分享」的那些事儿。分享时机，在不同的时机分析用户是否有分享的意愿，提供给他们合适的分享内容，能让分享的效果更好。用户分享内容到社交媒体或好友，不应该是一种粗暴的强制行为，我们应该在保证产品本身内容有吸引力的核心前提下，仔细揣摩用户心理...
+          </div>
         </div>
       </div>
       <div class="project2">
-        <div class="upPage upPage2"></div>
-        <div class="underPage underPage2">
+        <div class="upPage2"></div>
+        <div class="underPage2">
           <div class="projectTitle">知乎APP</div>
+          <div class="content">
+            用户主动点击分享。绝大多数APP都是在详情页等需要分享的页面放置一个分享按钮，由用户自行选择分享。这种情况下用户是否分享主要是取决于产品内容本身，比如内容有趣或是有用。在一些APP中设计者会对这个分享按钮加一些鼓励的文案或着重的设计来促进用户分享。我们从分享时机、分享形式、分享动机、分享场景4个维度来聊聊「社交分享」的那些事儿。
+          </div>
         </div>
       </div>
       <div class="project3">
-        <div class="upPage upPage3"></div>
-        <div class="underPage underPage3">
+        <div class="upPage3"></div>
+        <div class="underPage3">
           <div class="projectTitle">以伴的项目名称</div>
+          <div class="content">
+            项目的详细介绍分享形式、分享动机、分享场景4个维度来聊聊「社交分享」的那些事儿。分享时机，在不同的时机分析用户是否有分享的意愿，提供给他们合适的分享内容，能让分享的效果更好。用户分享内容到社交媒体或好友，不应该是一种粗暴的强制行为，我们应该在保证产品本身内容有吸引力的核心前提下，仔细揣摩用户心理...
+          </div>
         </div>
       </div>
     </div>
 
     <div class="cooperationPartner">
       <div class="partnerTitle">合作伙伴</div>
-      <img
-        class="partnerImg"
+      <img class="partnerImg"
         src="https://img.js.design/assets/img/6378c6dd6e9a5c7685b46cb0.png#de5e646163aafe80259239543453a409"
-        alt="图片加载失败"
-      />
+        alt="图片加载失败" />
     </div>
   </div>
 </template>
@@ -102,10 +110,12 @@ export default {};
   @extend %common-flex;
   width: 100%;
   min-height: 100vh;
+
   .con {
     @extend %common-flex;
     width: 100%;
     max-width: 1000px; //版心
+
     @media (max-width: 750%) {
       // 移动端适配
       max-width: unset;
@@ -114,7 +124,7 @@ export default {};
     }
 
     //  尽量使用子代选择器'>'
-    > .item,
+    >.item,
     p {
       margin: 0;
     }
@@ -122,506 +132,344 @@ export default {};
 }
 
 // -------------------------------
+$position: absolute;
+
+%align {
+  text-align: left;
+  vertical-align: top;
+}
+
+//color
+$color255-1: rgba(255, 255, 255, 1);
+$color255-04: rgba(255, 255, 255, 0.4);
+$color128-1: rgba(128, 128, 128, 1);
+$color77-1: rgba(77, 77, 77, 1);
+
+// font-family
+$family1: 阿里妈妈数黑体;
+$family2: 阿里巴巴惠普体regular;
+$family3: 思源黑体;
+$family4: 优设标题圆;
+
+@mixin size-position($width: auto, $height: auto, $left: auto, $top: auto) {
+  width: $width;
+  height: $height;
+  left: $left;
+  top: $top;
+}
+
 
 .picture {
-  left: 0px;
-  top: -105px;
-  width: 1440px;
-  height: 960px;
-  opacity: 1;
+  @include size-position(1440px, 960px, 0, -105px);
+  position: $position;
 
-  /* overflow: hidden; */
-  background-image: url("https://img.js.design/assets/img/636514531beac627deafba54.jpg#e228673ae2cf533f1f33bd1393df4df0");
-  background-size: 100% 100%;
-  position: absolute;
-  /* top: 105px; */
-}
+  background: {
+    image: url("https://img.js.design/assets/img/636514531beac627deafba54.jpg#e228673ae2cf533f1f33bd1393df4df0");
+    size: 100% 100%;
+  }
 
-.border {
-  left: 142px;
-  /* top: 334px; */
-  width: 523px;
-  height: 9px;
-  opacity: 1;
-  background: rgba(238, 106, 107, 1);
+  >.border {
+    @include size-position(523px, 9px, 142px, 439px);
+    background: rgba(238, 106, 107, 1);
+    position: $position;
+  }
 
-  position: absolute;
-  /* 以picture为参照 */
-  top: 439px;
-}
+  >.noteBackground {
+    @include size-position(523px, 305px, 142px, 448px);
+    background: rgba(18, 18, 18, 0.45);
+    position: $position;
 
-.noteBackground {
-  left: 142px;
-  /* top: 343px; */
-  width: 523px;
-  height: 305px;
-  opacity: 1;
-  background: rgba(18, 18, 18, 0.45);
 
-  position: absolute;
-  /* 以picture为参照 */
-  top: 448px;
-}
+    >.slogen1 {
+      @include size-position(300px, 144px, 49px, 32px);
+      @extend %align;
+      font: 400 60px/72px $family1;
+      color: $color255-1;
+      position: $position;
+    }
 
-.slogen1 {
-  /* left: 191px;
-    top: 375px; */
-  width: 300px;
-  height: 144px;
-  opacity: 1;
-  /** 文本1 */
-  font-size: 60px;
-  font-weight: 400;
-  letter-spacing: 0px;
-  /* line-height: 0px; */
-  color: rgba(255, 255, 255, 1);
-  text-align: left;
-  vertical-align: top;
+    >.slogen2 {
+      @include size-position(414px, 28px, 49px, 193px);
+      @extend %align;
+      // font: 400 20px/28px $family2;
+      font: 400 18px/28px $family2;//未设字体
+      color: $color255-1;
+      position: $position;
+    }
 
-  position: absolute;
-  line-height: 72px;
-  /* 以noteBackground为参照 */
-  left: 49px;
-  top: 32px;
-  font-family: 阿里妈妈数黑体;
-}
+    >.learnBackground {
+      @include size-position(139px, 45px, 49px, 238px);
+      background: rgba(219, 123, 123, 1);
+      position: $position;
 
-.slogen2 {
-  /* left: 191px;
-    top: 536px; */
-  width: 414px;
-  height: 28px;
-  opacity: 1;
-  /** 文本1 */
-  font-size: 20px;
-  font-weight: 400;
-  letter-spacing: 0px;
-  /* line-height: 0px; */
-  color: rgba(255, 255, 255, 1);
-  text-align: left;
-  vertical-align: top;
+      >.learnMore {
+        @include size-position(99px, 28px, 8px, 9px);
+        @extend %align;
+        // font: 400 20px/28px $family2;
+        font: 400 19px/28px $family2;//未设字体
+        letter-spacing: 5px;
+        color: $color255-1;
+        position: $position;
+      }
 
-  position: absolute;
-  line-height: 28px;
-  /* 以noteBackground为参照 */
-  left: 49px;
-  top: 193px;
-  /* font-family: 阿里巴巴普惠体thin; */
-  font-family: 阿里巴巴惠普体regular;
-}
+      >.arrow {
+        @include size-position(24px, 24px, 107px, 11px);
+        position: $position;
+      }
 
-.learnBackground {
-  /* left: 191px;
-    top: 581px; */
-  width: 139px;
-  height: 45px;
-  opacity: 1;
-  background: rgba(219, 123, 123, 1);
+      // .right{
+      //   border:9px solid $color255-1;
+      //   width: 0;
+      //   height: 0;
+      //   border-color: transparent transparent transparent $color255-1 ;
+      //   left: 9px;
+      //   top: 6px;
+      // }
+      .right {
+        @include size-position(9px, 12px, 9px, 6px);
+        background: $color255-1;
+        position: $position;
+      }
+    }
+  }
 
-  position: absolute;
-  /* 以noteBackground为参照 */
-  left: 49px;
-  top: 238px;
-}
+  %roller {
+    width: 50px;
+    height: 6px;
+    background: $color255-04;
+    position: $position;
+    top: 910px;
+  }
 
-.learnMore {
-  /* left: 199px;
-    top: 590px; */
-  width: 99px;
-  height: 28px;
-  opacity: 1;
-  /** 文本1 */
-  font-size: 20px;
-  font-weight: 500;
-  letter-spacing: 5px;
-  /* line-height: 0px; */
-  color: rgba(255, 255, 255, 1);
-  text-align: left;
-  vertical-align: top;
+  >.roller1 {
+    @extend %roller;
+    left: 1153px;
+  }
 
-  position: absolute;
-  line-height: 28px;
-  /* 以learnBackground为参照 */
-  left: 8px;
-  top: 9px;
-  font-family: 阿里巴巴惠普体regular;
-}
+  >.roller2 {
+    @extend %roller;
+    left: 1213px;
+  }
 
-.arrow {
-  /* arrow-right-fill */
-  /* left: 298px;
-    top: 592px; */
-  width: 24px;
-  height: 24px;
-  opacity: 1;
-
-  /* 以learnBackground为参照 */
-  left: 107px;
-  top: 11px;
-  /* background-color: aqua; */
-  position: absolute;
-}
-
-/* .right {
-    left: 307px;
-    top: 598px;
-    width: 9px;
-    height: 12px;
-    opacity: 1;
-    background: rgba(255, 255, 255, 1); */
-
-/* 以arrow为参照 */
-/* left: 9px;
-    top: 6px;
-    position: absolute;
-} */
-
-.roller1 {
-  /* left: 1153px;
-    top: 805px; */
-  width: 50px;
-  height: 6px;
-  opacity: 1;
-  background: rgba(255, 255, 255, 0.4);
-
-  position: absolute;
-  left: 1153px;
-  top: 910px;
-}
-
-.roller2 {
-  /* left: 1213px;
-    top: 805px; */
-  width: 50px;
-  height: 6px;
-  opacity: 1;
-  background: rgba(255, 255, 255, 1);
-
-  position: absolute;
-  left: 1213px;
-  top: 910px;
-}
-
-.roller3 {
-  /* left: 1273px;
-    top: 805px; */
-  width: 50px;
-  height: 6px;
-  opacity: 1;
-  background: rgba(255, 255, 255, 0.4);
-
-  position: absolute;
-  left: 1273px;
-  top: 910px;
+  >.roller3 {
+    @extend %roller;
+    left: 1273px;
+  }
 }
 
 .aboutUs {
-  left: 0px;
-  top: 855px;
-  width: 720px;
-  height: 815px;
-  opacity: 1;
+  @include size-position(720px, 764px, 0, 855px);
   background: rgba(238, 106, 107, 1);
+  position: $position;
 
-  position: absolute;
-}
+  >.aboutTitle {
+    @include size-position(192px, 58px, 69px, 70px);
+    @extend %align;
+    font: 400 48px/58px $family1;
+    color: $color255-1;
+    position: $position;
+  }
 
-.aboutTitle {
-  /* left: 69px;
-    top: 925px; */
-  width: 192px;
-  height: 58px;
-  opacity: 1;
-  /** 文本1 */
-  font-size: 48px;
-  font-weight: 400;
-  letter-spacing: 0px;
-  /* line-height: 0px; */
-  color: rgba(255, 255, 255, 1);
-  text-align: left;
-  vertical-align: top;
-
-  position: absolute;
-  line-height: 58px;
-  left: 69px;
-  top: 70px;
-  font-family: 阿里妈妈数黑体;
-}
-
-.article {
-  /* left: 69px;
-    top: 1040px; */
-  width: 534px;
-  height: 514px;
-  opacity: 1;
-  font-size: 18px;
-  font-weight: 400;
-  letter-spacing: 0px;
-  /* line-height: 0px; */
-  color: rgba(255, 255, 255, 1);
-  text-align: left;
-  vertical-align: top;
-
-  position: absolute;
-  line-height: 25.7px;
-  left: 69px;
-  top: 185px;
-  white-space: pre-wrap;
-  /* word-break: keep-all; */
-  font-family: 思源黑体;
+  >.article {
+    @include size-position(534px, 514px, 69px, 185px);
+    @extend %align;
+    font: 400 18px/25.7px $family3;
+    color: $color255-1;
+    position: $position;
+    white-space: pre-wrap;
+  }
 }
 
 .numberBackground {
-  left: 720px;
-  top: 855px;
-  width: 720px;
-  height: 815px;
-  opacity: 1;
-  /* background: rgba(135, 135, 135, 0); */
+  @include size-position(720px, 764px, 720px, 855px);
+  position: $position;
 
-  /* background-color: rgb(11, 6, 6); */
-  position: absolute;
-}
+  %bigNumber {
+    @extend %align;
+    font: 400 72px/94px $family4;
+    letter-spacing: 5px;
+    color: rgba(47, 46, 52, 1);
+    position: $position;
+  }
 
-.bigNumber {
-  height: 94px;
-  opacity: 1;
-  /** 文本1 */
-  font-size: 72px;
-  font-weight: 400;
-  letter-spacing: 5px;
-  /* line-height: 0px; */
-  color: rgba(47, 46, 52, 1);
-  text-align: left;
-  vertical-align: top;
+  %smallNumber {
+    @extend %align;
+    font: 400 14px/21px $family3;
+    color: rgba(166, 166, 166, 1);
+    position: $position;
+  }
 
-  position: absolute;
-  line-height: 94px;
-  font-family: 优设标题圆;
-}
+  >.number1 {
+    @extend %bigNumber;
+    @include size-position(auto, 94px, 261px, 133px);
+  }
 
-.smallNumber {
-  height: 21px;
-  opacity: 1;
-  /** 文本1 */
-  font-size: 14px;
-  font-weight: 400;
-  letter-spacing: 0px;
-  /* line-height: 0px; */
-  color: rgba(166, 166, 166, 1);
-  text-align: left;
-  vertical-align: top;
+  >.number11 {
+    @extend %smallNumber;
+    @include size-position(auto, 21px, 322px, 217px);
+  }
 
-  position: absolute;
-  font-family: 思源黑体;
-  line-height: 21px;
-}
+  >.number2 {
+    @extend %bigNumber;
+    @include size-position(auto, 94px, 286px, 357px);
+  }
 
-.number1 {
-  top: 133px;
-  left: 261px;
-  width: 258px;
-}
+  >.number21 {
+    @extend %smallNumber;
+    @include size-position(auto, 21px, 322px, 441px);
+  }
 
-.number11 {
-  width: 137px;
-  left: 322px;
-  top: 217px;
-}
+  >.number3 {
+    @extend %bigNumber;
+    @include size-position(auto, 94px, 315px, 584px);
+  }
 
-.number2 {
-  left: 286px;
-  top: 357px;
-  width: 207px;
-}
-
-.number21 {
-  left: 322px;
-  top: 441px;
-  width: 144px;
-}
-
-.number3 {
-  left: 315px;
-  top: 584px;
-  width: 149px;
-}
-
-.number31 {
-  left: 286px;
-  top: 668px;
-  width: 206px;
+  >.number31 {
+    @extend %smallNumber;
+    @include size-position(auto, 21px, 286px, 668px);
+  }
 }
 
 .coreTitle {
-  left: 69px;
-  top: 1684px;
-  width: 336px;
-  height: 58px;
-  opacity: 1;
-  /** 文本1 */
-  font-size: 48px;
-  font-weight: 400;
-  letter-spacing: 0px;
-  /* line-height: 0px; */
+  @include size-position(336px, 58px, 69px, 1684px);
+  @extend %align;
+  font: 400 48px/58px $family1;
   color: rgba(47, 46, 52, 1);
-  text-align: left;
-  vertical-align: top;
-
-  position: absolute;
-  line-height: 58px;
-  font-family: 阿里妈妈数黑体;
+  position: $position;
 }
 
-.spread {
+%spread {
   width: 108px;
   height: 420px;
-  opacity: 1;
-
-  position: absolute;
+  top: 1815px;
+  position: $position;
 }
 
 .spread01 {
-  left: 70px;
-  top: 1814px;
-  background: rgba(128, 128, 128, 1);
+  @include size-position(630px, 420px, 69px, 1815px);
+  position: $position;
+  background: rgba(238, 106, 107, 0.85);
+
 }
 
 .spread02 {
+  @extend %spread;
   left: 720px;
-  top: 1815px;
-  background: rgba(128, 128, 128, 1);
+  background: $color128-1;
 }
 
 .spread03 {
+  @extend %spread;
   left: 849px;
-  top: 1815px;
   background: rgba(144, 144, 144, 1);
+
 }
 
 .spread04 {
+  @extend %spread;
   left: 978px;
-  top: 1815px;
   background: rgba(160, 160, 160, 1);
 }
 
 .spread05 {
+  @extend %spread;
   left: 1107px;
-  top: 1814px;
   background: rgba(176, 176, 176, 1);
 }
 
 .spread06 {
+  @extend %spread;
   left: 1236px;
-  top: 1815px;
   background: rgba(192, 192, 192, 1);
 }
 
 .runningTitle {
-  left: 69px;
-  top: 2435px;
-  width: 432px;
-  height: 58px;
-  opacity: 1;
-  /** 文本1 */
-  font-size: 48px;
-  font-weight: 400;
-  letter-spacing: 0px;
-  /* line-height: 0px; */
-  color: rgba(77, 77, 77, 1);
-  text-align: left;
-  vertical-align: top;
-
-  position: absolute;
-  line-height: 58px;
-  font-family: 阿里妈妈数黑体;
+  @include size-position(432px, 58px, 69px, 2435px);
+  @extend %align;
+  font: 400 48px/58px $family1;
+  color: $color77-1;
+  position: $position;
 }
 
-.upPage {
+%upPage {
   top: 2590px;
   width: 359px;
   height: 189px;
-  opacity: 1;
   background-image: url(https://img.js.design/assets/smartFill/img348164da74c4b8.jpeg);
   background-color: rgba(204, 204, 204, 1);
-
-  position: absolute;
+  position: $position;
   background-size: 100% 100%;
 }
 
-.underPage {
+%underPage {
   top: 2779px;
   width: 359px;
   height: 280px;
-  opacity: 1;
   background: rgba(238, 106, 107, 1);
-
-  position: absolute;
+  position: $position;
 }
 
-.upPage1,
+$left1: 87px;
+$left2: 541px;
+$left3: 995px;
+
+.upPage1 {
+  @extend %upPage;
+  left: $left1;
+}
+
 .underPage1 {
-  left: 87px;
+  @extend %underPage;
+  left: $left1;
 }
 
-.upPage2,
+.upPage2 {
+  @extend %upPage;
+  left: $left2;
+}
+
 .underPage2 {
-  left: 541px;
+  @extend %underPage;
+  left: $left2;
 }
 
-.upPage3,
+.upPage3 {
+  @extend %upPage;
+  left: $left3;
+}
+
 .underPage3 {
-  left: 995px;
+  @extend %underPage;
+  left: $left3;
 }
 
 .projectTitle {
-  /* width: 166px; */
-  height: 33px;
-  opacity: 1;
-  /** 文本1 */
-  font-size: 24px;
-  font-weight: 700;
-  letter-spacing: 0px;
-  /* line-height: 0px; */
-  color: rgba(255, 255, 255, 1);
-  text-align: left;
-  vertical-align: top;
+  @include size-position(auto, 33px, 25px, 28px);
+  @extend %align;
+  font: 700 24px/33px $family2;
+  color: $color255-1;
+  position: $position;
+}
 
-  width: auto;
-  line-height: 33px;
-  position: absolute;
-  font-family: 阿里巴巴普惠体regular;
-  left: 25px;
-  top: 28px;
+.content {
+  @include size-position(294px, 170px, 25px, 74px);
+  font: 400 14px/21.25px $family3;
+  @extend %align;
+  color: rgba(255, 255, 255, 1);
+  position: $position;
+
 }
 
 .partnerTitle {
-  left: 87px;
-  top: 3146px;
-  width: 192px;
-  height: 58px;
-  opacity: 1;
-  /** 文本1 */
-  font-size: 48px;
-  font-weight: 400;
-  letter-spacing: 0px;
-  /* line-height: 0px; */
-  color: rgba(77, 77, 77, 1);
-  text-align: left;
-  vertical-align: top;
-
-  position: absolute;
-  line-height: 58px;
-  font-family: 阿里妈妈数黑体;
+  @include size-position(192px, 58px, 87px, 3146px);
+  @extend %align;
+  font: 400 48px/58px $family1;
+  color: $color77-1;
+  position: $position;
 }
 
 .partnerImg {
-  left: 135px;
-  top: 3237px;
-  width: 1219px;
-  height: 245px;
-  opacity: 1;
-
-  position: absolute;
+  @include size-position(1219px, 245px, 135px, 3237px);
+  position: $position;
 }
 </style>
